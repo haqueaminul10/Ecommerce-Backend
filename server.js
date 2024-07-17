7838a04d281ee3b65c6d7a6b93fbfdd8b8175425
haqueaminul10100 <aminul@thedoodleinc.com>
2024-07-17 13:38:51 +0600

    project setup

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
-console.log('Ecommerce BackEnd');
+//REQUIRE EXPRESS
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+//DOTENV
+const dotenv = require('dotenv');
+dotenv.config();
+//PORT
+const PORT = process.env.PORT;
+//SERVER LISTEN
+app.listen(PORT, () => {
+  console.log(`server is running ${PORT}`);
+});
